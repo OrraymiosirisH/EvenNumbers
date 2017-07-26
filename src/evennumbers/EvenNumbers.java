80897cbecd7d63f0bf8ede98b284c45a0314289c
OrraymiosirisH <ohguevara.alvarez@gmail.com>
2017-07-26 15:50:19 -0400

    odd and even#

---
+++ src/evennumbers/EvenNumbers.java
@@ -0,0 +1,23 @@
+package evennumbers;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int x=0;x<=99;x++)
+			
+			if((x%2)==0){
+			
+			System.out.println(x+1);
+		
+			
+			}
+		    else{
+		    	
+		    	System.out.println(x+1 + " Is an even #");
+		    }
+		
+	}
+
+}
